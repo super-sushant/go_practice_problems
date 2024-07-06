0d695ecaf82a1ddee71d6c2e7e9acf5853c3fd8b
Sushant Sharma <supersushant7@gmail.com>
2024-07-06 19:15:18 +0530

    prob11 unused vars

---
+++ Prob11_ignoring_values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main () {
+	firstname, lastname := getNames()
+
+	fmt.Printf("Hi %v ass boi",firstname)
+
+}
+
+func getNames()(string,string){
+	return "Bitch", "lasagna"
+}
