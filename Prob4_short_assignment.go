ec0578d163f2d16b5ab6c852e6f37e59f3fd6cf5
Sushant Sharma <supersushant7@gmail.com>
2024-07-06 16:09:22 +0530

    fix: prob4 short assignment

--- Prob4_short_assignment.go
+++ Prob4_short_assignment.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main (){
-	var smsSendingLimit int 
-	var costPerSMS float64
-	var hasPermission bool
-	var username string
+	smsSendingLimit := 0 
+	costPerSMS := 0.00 
+	hasPermission := false
+	username := ""
 
 	fmt.Printf("%v %f %v %q \n",smsSendingLimit,costPerSMS,hasPermission,username)
 }
