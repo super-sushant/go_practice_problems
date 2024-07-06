697aeb76884bd118da53f85cf85982a6cc1dcd25
Sushant Sharma <supersushant7@gmail.com>
2024-07-06 16:17:53 +0530

    fix: prob6 type convert to int

--- Prob6_type_conversion.go
+++ Prob6_type_conversion.go
@@ -5,5 +5,5 @@ import "fmt"
 func main () {
 	accountAge:= 2.5
 
-	fmt.Println("Your account has existed for ",accountAge,"years.")
+	fmt.Println("Your account has existed for ", uint(accountAge),"years.")
 }
