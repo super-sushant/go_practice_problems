221cf12c48cb6d66c3c90dfc73ebd2858e9f3f8d
Sushant Sharma <supersushant7@gmail.com>
2024-07-06 19:11:09 +0530

    fix:prob10 increment

--- Prob10_pass_by_value.go
+++ Prob10_pass_by_value.go
@@ -5,15 +5,15 @@ import "fmt"
 func main() {
 	x := 10
 
-	inc(x)
+	x=inc(x)
 
 	fmt.Println(x,"Ouside increment")
 
 
 }
 
-func inc(x int) {
+func inc(x int)int {
 	x = x * 2
 	fmt.Println(x,"Inside increment")
-
+	return x
 }
