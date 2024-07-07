9c1d0fa0bd809985fe30715024ca295f30a02bae
Sushant Sharma <supersushant7@gmail.com>
2024-07-07 11:17:56 +0530

    fix: prob18 maps

--- Prob18/Prob18_maps.go
+++ Prob18/Prob18_maps.go
@@ -1,13 +1,27 @@
 package main
 
-func geUserMap(names []string, numbers []int) map[string]int {
+import "fmt"
 
-}
+
 
 func main(){
-	name:= []string{"Shakti","Bhakti","Rakti"}
+	name:= []string{"Shakti","Bhakti","Rakti","Nakti"}
 	numbers:= []int{12,13,14}
 	userDict := getUserMap(name,numbers)
 
-	print(userDict)
+	fmt.Print(userDict)
+}
+
+func getUserMap(names []string, numbers []int) (myMap map[string]int) {
+	myMap= make(map[string] int)
+
+	for i,name :=range names {
+		if len(numbers)>i {
+			myMap[name]= numbers[i]
+		}else {
+			myMap[name]=0
+		}
+	}
+	
+	return myMap
 }
