7626f1a8c2d0fbf8f7c93a7f2ee9c0c41280cc14
Sushant Sharma <supersushant7@gmail.com>
2024-07-14 18:48:22 +0530

    fix:prob20 nested count map

--- Prob20/Prob20_nested_maps.go
+++ Prob20/Prob20_nested_maps.go
@@ -4,6 +4,21 @@ import "fmt"
 
 func getNamesCount(userIds []string) (nameCountsMap map[string]map[string]int){
 	nameCountsMap = make(map[string]map[string]int)
+
+	for _,name := range(userIds) {
+		firstChar:=string([]rune(name)[0])
+		namesMap,ok := nameCountsMap[firstChar]
+		if ok {
+			count:= namesMap[name]
+			// necessary value from retrieve is not referenced to the map
+			nameCountsMap[firstChar][name] = count+1
+		}else {
+			nameCountsMap[firstChar]= make(map[string]int)
+			nameCountsMap[firstChar][name] = 1
+			
+		}
+	}
+
 	return nameCountsMap
 }
 
