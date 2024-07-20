244866de042536f2971b4f4b8077077d9e57edc8
Sushant Sharma <supersushant7@gmail.com>
2024-07-20 20:27:50 +0530

    fix:prob21 go routine

--- Prob21/Prob21_go_routines.go
+++ Prob21/Prob21_go_routines.go
@@ -2,29 +2,27 @@ package main
 
 import "fmt"
 
-
-func waitForDbs(numDBs int,dbChan chan struct{}) {
-	
+func waitForDbs(numDBs int, dbChan chan struct{}) {
+	for i:=0; i<numDBs; i++ {
+		<-dbChan
+	}
 }
-
 
 func getDatabasesChannel(numDbs int) chan struct{} {
 	ch := make(chan struct{})
 
-	go func() {
-		for i:=0; i< numDBs; i++ {
-			ch <-struct{}{}
-			fmt.Printf("Data %v is online \n",i+1)
+	go func(numDBs int) {
+		for i := 0; i < numDBs; i++ {
+			ch <- struct{}{}
+			fmt.Printf("Data %v is online \n", i+1)
 		}
-	}()
+	}(numDbs)
 
 	return ch
 }
 
+func main() {
+	dbChan := getDatabasesChannel(10)
 
-
-func main (){
-	dbChan:=getDatabasesChannel(10)
-
-	waitForDbs(10,dbChan)
+	waitForDbs(10, dbChan)
 }
