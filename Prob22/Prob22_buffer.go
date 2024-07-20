070295c57bb4ddef1b0bdc55ac9d06ac70f4b16b
Sushant Sharma <supersushant7@gmail.com>
2024-07-20 22:04:22 +0530

    prob22 dead lock and buffer

---
+++ Prob22/Prob22_buffer.go
@@ -0,0 +1,26 @@
+package main 
+
+func addEmailToQueue(emails []string ) chan string {
+	emailsToSend := make(chan string)
+
+	for _,email :=range emails {
+		emailsToSend <- email
+	}
+	return emailsToSend
+}
+
+func sendEmails(batchSize int, ch chan string) {
+	for i:= 0; i< batchSize; i++ {
+		email:= <-ch
+		println("Sending email:",email)
+	}
+}
+
+func main () {
+	println("Adding email to queue")
+
+	ch:=addEmailToQueue([]string{"hi","hello","email2","email3"})
+
+	println("Sending emails")
+	sendEmails(4,ch)
+}
